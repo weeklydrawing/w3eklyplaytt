fcff997caa7164b435f5b72c89d937126f8c7b5a
W3ekly_Play_TT <113057748+weeklydrawing@users.noreply.github.com>
2025-12-01 19:04:58 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import cover from './images/cover.png';
 import stop from './song/stopplayin.mp3';
 import bg from './videos/car.mp4';
 import git from './images/git2.png';
-import tiktok from './image/tiktok.png';
+import tiktok from './images/tiktok.png';
 
 function App() {
   const [viewCount, setViewCount] = useState(5);
